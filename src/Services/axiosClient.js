d1266bb5e16c9c2ddc59f6be44fa585ae062a744
EzequielPN1 <ezequiel.sitepro@gmail.com>
2023-06-17 11:26:37 -0300

    Ok

--- src/Services/axiosClient.js
+++ src/Services/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3000',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
